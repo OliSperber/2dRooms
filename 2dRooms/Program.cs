fcc32bc8ff0beac339edb7c0e19b062bd74b3f91
OliSperber <o.sperber@student.avans.nl>
2025-03-06 14:03:56 +0100

    Test

--- 2dRooms/Program.cs
+++ 2dRooms/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.AspNetCore.Authentication.BearerToken;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -9,6 +13,28 @@ builder.Services.AddSwaggerGen();
 
 var sqlConnectionString = builder.Configuration.GetValue<string>("SqlConnectionString");
 var sqlConnectionStringFound = !string.IsNullOrWhiteSpace(sqlConnectionString);
+
+builder.Services.AddAuthorization();
+builder.Services
+    .AddIdentityApiEndpoints<IdentityUser>(options =>
+    {
+        options.Password.RequireDigit = true;
+        options.Password.RequiredLength = 8;
+        options.Password.RequireLowercase = true;
+        options.Password.RequireUppercase = true;
+        options.Password.RequireNonAlphanumeric = true;
+    })
+    .AddDapperStores(options =>
+    {
+        options.ConnectionString = sqlConnectionString;
+    });
+builder.Services
+    .AddOptions<BearerTokenOptions>(IdentityConstants.BearerScheme)
+    .Configure(options =>
+    {
+        options.BearerTokenExpiration = TimeSpan.FromMinutes(60);
+    });
+
 
 var app = builder.Build();
 
