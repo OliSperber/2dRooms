2cbedb2babb9ba2afd90721a2af5d10c63069a5b
OliSperber <o.sperber@student.avans.nl>
2025-03-08 13:23:33 +0100

    new models

---
+++ 2dRooms/Models/Environment2D.cs
@@ -0,0 +1,12 @@
+﻿namespace _2dRooms.Models
+{
+    public class Environment2D
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public float MaxHeight { get; set; }
+        public float MaxWidth { get; set; }
+        public Guid UserId { get; set; }
+        
+    }
+}
